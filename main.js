cfc3b9380bc0bcc2bd9d28789456482fb53fd02d
winston kiprop <winstonekip50@gmail.com>
2022-06-20 11:08:46 +0300

    Done.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
@@ -6,20 +6,30 @@ const FULL_HEART = '♥'
 
 
 
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
+document.addEventListener('DOMContentLoaded',()=>{
+  const likers = document.querySelectorAll('span.like-glyph')
+  likers.forEach(liker=>{
+    liker.addEventListener('click',()=>{
+      if (liker.innerText === FULL_HEART ) {
+        liker.classList.remove('activated-heart')
+        liker.innerText = EMPTY_HEART
+      }else{
+        mimicServerCall()
+        .then(()=>{
+          liker.innerText = FULL_HEART
+          liker.classList.add('activated-heart')
+        })
+        .catch(()=>{
+          let error = document.getElementById('modal')
+          error.classList.remove('hidden')
+          let message = document.getElementById('modal-message')
+          message.innerText = "Random server error. Try again."
+          setTimeout(()=>{
+            error.classList.add('hidden')
+          },3000)
+        })
       }
-    }, 300);
-  });
-}
+    })
+  })
+})
+
